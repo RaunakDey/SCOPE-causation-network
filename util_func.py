2ae000cd07fb0ba29a71878f8a90264dbaaa55f4
RaunakDey <deyraunak111@gmail.com>
2023-11-17 14:22:36 -0500

    preprocessing TS

--- util_func.py
+++ util_func.py
@@ -4,25 +4,10 @@ import pandas as pd
 import numpy as np
 
 
-import pandas as pd
-
-def replace_negative_with_mean(df, cols):
-    for col in cols:
-        # Calculate mean excluding values less than or equal to 0
-        mean_value = df[df[col] > 0][col].mean()
-        
-        # Replace values less than or equal to 0 with the calculated mean
-        df[col] = df[col].apply(lambda x: mean_value if x <= 0 else x)
-    
-    return df
 
 
 
-# time_series_utils.py
-
-import pandas as pd
-
-def preprocess_time_series(df, date_column='ds', value_column='y', target_frequency='M', interpolation_method='linear', fillna_value=None):
+def preprocess_time_series(df, date_column='ds', value_column='y', target_frequency='M', interpolation_method='linear', remove_negative = True, fillna_value=None):
     """
     Preprocess time series data with irregular time intervals.
 
@@ -36,6 +21,8 @@ def preprocess_time_series(df, date_column='ds', value_column='y', target_freque
 
     Returns:
     - Preprocessed pandas DataFrame with a fixed frequency time series.
+    (c) Raunak Dey -- SCOPE causation network
+
     """
     # Convert the date_column to datetime type
     df[date_column] = pd.to_datetime(df[date_column])
@@ -43,12 +30,16 @@ def preprocess_time_series(df, date_column='ds', value_column='y', target_freque
     # Set the date_column as the index
     df.set_index(date_column, inplace=True)
 
+    # remove negative values
+    if remove_negative is True:
+        df[value_column] = df[value_column].apply(lambda x: float("nan") if x < 0 else x)
+
     # Resample the time series data to the target frequency
-    df_resampled = df.resample(target_frequency).sum()  # Using sum as an example, you can change it to your aggregation function
+    df_resampled = df.resample(target_frequency).mean()  # Using sum as an example, you can change it to your aggregation function
 
     # Fill missing values if specified
-    if fillna_value is not None:
-        df_resampled[value_column].fillna(fillna_value, inplace=True)
+    if fillna_value is None:
+        df_resampled[value_column].fillna(float("NaN"), inplace=True)
 
     # Interpolate missing values
     df_resampled[value_column] = df_resampled[value_column].interpolate(method=interpolation_method)
@@ -57,3 +48,50 @@ def preprocess_time_series(df, date_column='ds', value_column='y', target_freque
     df_resampled.reset_index(inplace=True)
 
     return df_resampled
+
+
+
+def replace_negative_with_mean(df, cols):
+    """
+    Replace the zeros and negative data of the frame with means of the non-zero elements
+
+    Parameters:
+    -- df: pandas DataFrame containing time series data.
+    -- cols: list of the columns in the dataframe for which the operation is performed
+    
+    Returns:
+    - Processed pandas DataFrame with a greater than zero time series
+
+    (c) Raunak Dey -- SCOPE causation network
+
+    """
+
+    for col in cols:
+        # Calculate mean excluding values less than or equal to 0
+        mean_value = df[df[col] > 0][col].mean()
+        
+        # Replace values less than or equal to 0 with the calculated mean
+        df[col] = df[col].apply(lambda x: mean_value if x <= 0 else x)
+    
+    return df
+
+
+
+def time_series_equalfreq(df, date_column='ds', value_column='y'):
+    '''
+    Resample the time series so that the final time series becomes uni -- frequency, by upscaling.
+
+    Parameters:  
+    - df: pandas dataframe on which the operation is performed
+    - date_column : for which the dates are setected to be performed upon
+    - value_column: the column which is upscaled
+
+    Returns:
+    - df_processed : final dataframe with processed time series. 
+    '''
+    
+    #upsampled = df.resample('D')
+    #interpolated = upsampled.interpolate(method='spline', order=2)
+
+    return 0
+
